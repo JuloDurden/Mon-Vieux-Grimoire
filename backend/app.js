321ef1d34a3af27a89b8320a19965992f722744f
JuloDurden <juloclavel@gmail.com>
2025-05-22 11:15:00 +0200

    Added PUT & DELETE Routes

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.post('/api/books', (req, res, next) => {
     ...req.body
   });
   book.save()
-    .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+    .then(() => res.status(201).json({ message: 'Votre livre est enregistré'}))
     .catch(error => res.status(400).json({ error }));
 });
 
@@ -34,6 +34,18 @@ app.get('/api/books/:id', (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then(book => res.status(200).json(book))
     .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then (() => res.status(200).json({ message: 'Votre livre est modifié'}))
+    .catch(error => res.status(400).json({ error }));
+})
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 app.get('/api/books', (req, res, next) => {
