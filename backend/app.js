cd3010e42119a6148396f538c764af36ad9fcb02
JuloDurden <juloclavel@gmail.com>
2025-05-22 15:53:00 +0200

    Optimization Time
    Splitted App.js into JS files in controllers & routes folders.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Book = require('./models/book');
+const bookRoutes = require('./routes/book.routes');
 
 mongoose.connect('mongodb+srv://julowebdev:QWDszaJfUnhNZ4RD@cluster0.smxhaj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // Adresse de la MongoDB à changer
   { useNewUrlParser: true,
@@ -18,42 +18,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.post('/api/books', (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(() => res.status(201).json({ message: 'Votre livre est enregistré'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/books/:id', (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.put('/api/books/:id', (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then (() => res.status(200).json({ message: 'Votre livre est modifié'}))
-    .catch(error => res.status(400).json({ error }));
-})
-
-app.delete('/api/books/:id', (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Livre supprimé'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/books', (req, res, next) => {
-  Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
+app.use('/api/book', bookRoutes);
 
 module.exports = app;
